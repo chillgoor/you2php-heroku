d34265ead51168337a06149f048a48a70eaae0bf
chillgoor <chillgoor922@gmail.com>
2020-12-09 17:49:31 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,8 +1,8 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyDpwXwkkH0PouVWCcSHEtGtihUDsawTzHw');//这里改成您的api key
+define('APIKEY', 'AIzaSyCAlYXxr2tXONiFvmsoN4wKSHX58D-PxCk');//这里改成您的api key
 define('GJ_CODE', 'HK');//国家代码
-define('SITE_NAME', 'Chill-Gor');//网站名字
+define('SITE_NAME', '你的超哥');//网站名字
 define('TITLENAME', 'Chill-Tube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
 define('EMAIL', 'chill@hi2.in');//邮箱
